9dee3624b302cf42a7d614ebd141918cc6dd03a4
Maxim Butenko <maxim.butenko92.1@gmail.com>
2022-02-23 14:33:42 +0300

    fsdfsdf

--- src/data/sidebarItems.js
+++ src/data/sidebarItems.js
@@ -16,4 +16,34 @@ export const sidebarItems = [
 		username: "Марина Шевченко",
 		lastMessage: "Медвежатина, ти как? Я пишу очень-очень-очень-очень длинное сообщение",
 	},
+	{
+		avatarSrc: getAvatarSrc(),
+		username: "Альберт Абрамян",
+		lastMessage: "Здарова ебантяй. Как дела?",
+	},
+	{
+		avatarSrc: getAvatarSrc(),
+		username: "Александр Ильич",
+		lastMessage: "Привет ебантяи. Что делаете?",
+	},
+	{
+		avatarSrc: getAvatarSrc(),
+		username: "Марина Шевченко",
+		lastMessage: "Медвежатина, ти как? Я пишу очень-очень-очень-очень длинное сообщение",
+	},
+	{
+		avatarSrc: getAvatarSrc(),
+		username: "Альберт Абрамян",
+		lastMessage: "Здарова ебантяй. Как дела?",
+	},
+	{
+		avatarSrc: getAvatarSrc(),
+		username: "Александр Ильич",
+		lastMessage: "Привет ебантяи. Что делаете?",
+	},
+	{
+		avatarSrc: getAvatarSrc(),
+		username: "Марина Шевченко",
+		lastMessage: "Медвежатина, ти как? Я пишу очень-очень-очень-очень длинное сообщение",
+	},
 ];
