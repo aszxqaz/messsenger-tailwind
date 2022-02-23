1086266a9e52132e0798823f9c5241990557e9d5
Maxim Butenko <maxim.butenko92.1@gmail.com>
2022-02-23 03:16:04 +0300

    fsdfsdfd

--- src/containers/ChatMessage.jsx
+++ src/containers/ChatMessage.jsx
@@ -1,8 +1,9 @@
 import tw from 'tailwind-styled-components';
 
 const Wrapper = tw.div`
-    flex flex-col mb-2 relative w-fit h-fit max-w-xs py-2 px-3 bg-slate-100 rounded-xl text-sm
+    flex flex-col mb-2 relative w-fit h-fit max-w-xs py-2 px-3 rounded-xl text-sm
     ${p => p.self && 'self-end'}
+	${p => p.self ? 'bg-green-100' : 'bg-blue-200'}
 `;
 
 const MessageText = tw.div`
